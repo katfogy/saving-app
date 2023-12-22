8332b2641d66f923216252b0d0938f41a4e9cd19
katfogy <katfogy@gmail.com>
2023-12-22 21:48:09 +0100

    Default Root added

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "users#index"
+  # resources :Welcome
+  get "/index", to: "users#index"
 end
